189372985b0502066d9ace1c5cb17b1ed29b88b8
Dune Desormeaux <dune.desormeaux@gmail.com>
2024-12-17 17:41:30 -0800

    cv mix, s&h, clock, random skip all implemented. Issues with feedback patching, no quant yet

--- releases/12_goldfish/main.cpp
+++ releases/12_goldfish/main.cpp
@@ -1,49 +1,146 @@
 #include "ComputerCard.h"
+#include <cmath>
 
 /// Goldfish
 
 class Goldfish : public ComputerCard
 {
 public:
-	
 	virtual void ProcessSample()
 	{
-		
-		
-		
+			
+			
+			float amp1 = KnobVal(Knob::X) / 4095.0f;
+			float amp2 = KnobVal(Knob::Y) / 4095.0f;
+
+			int cvMix = 0;
+			int thing1 = 0;
+			int thing2 = 0;
+			bool clockPulse = false;
+			bool randPulse = false;
+
+			int32_t rand = LFrnd();
+
+			if (rand > (3000-KnobVal(Knob::X))) randPulse = true;
+
+			if(Connected(Input::CV1) && Connected(Input::CV2))
+			{
+				thing1 = int16_t(CVIn1()*amp1);
+				thing2 = int16_t(CVIn2()*amp2);
+			}
+			else if(Connected(Input::CV1))
+			{
+				thing1 = int16_t(CVIn1()*amp1);
+				thing2 = int16_t(2048 * (amp2 - 0.5));
+			}
+			else if(Connected(Input::CV2))
+			{
+				thing1 = int16_t(amp1 * rand);
+				thing2 = int16_t(CVIn2()*amp2);
+			} else {
+				thing1 = int16_t(amp1 * rand);
+				thing2 = int16_t(2048 *(amp2-0.5));
+			};
+
+			cvMix = (thing1 * (4095 - KnobVal(Knob::Main)) + thing2 * KnobVal(Knob::Main)) >> 12;
+
+			
+
+			CVOut1(cvMix);
+
+			clockRate = (4095 - KnobVal(Knob::Y)) << 3;
+			clockRate += (1 - amp2) * 24000 + 50;
+			
+
+			clock++;
+			if (clock>clockRate)
+			{
+				clock = 0;
+				PulseOut1(true);
+				LedOn(1, true);
+				pulseTimer1 = 200;
+				clockPulse = true;
+			};
+
+			if(Connected(Input::Pulse1))
+			{
+				if(PulseIn1RisingEdge())
+				{
+					CVOut2(cvMix);
+					LedOn(0, true);
+					PulseOut1(true);
+					pulseTimer1 = 200;
+					if(randPulse)
+					{
+						PulseOut2(true);
+						pulseTimer2 = 200;
+					}
+				};
+			}
+			else
+			{
+				if(clockPulse)
+				{
+					CVOut2(cvMix);
+					if(randPulse)
+					{
+						PulseOut2(true);
+						pulseTimer2 = 200;
+					}
+				}
+			};
 
 
-		
+			// If a pulse is ongoing, keep counting until it ends
+			if (pulseTimer1)
+			{
+				pulseTimer1--;
+				if (pulseTimer1==0) // pulse ends
+				{
+					PulseOut1(false);
+					LedOn(1, false);
+					LedOn(0, false);
+				}
+			};
 
-		//old code from passthrough
-		// // Transfer audio/CV/Pulse inputs directly to outputs
-		// AudioOut1(AudioIn1());
-		// AudioOut2(AudioIn2());
+			// If a pulse is ongoing, keep counting until it ends
+			if (pulseTimer2)
+			{
+				pulseTimer2--;
+				if (pulseTimer2==0) // pulse ends
+				{
+					PulseOut2(false);
+				}
+			};
 
-		// CVOut1(CVIn1());
-		// CVOut2(CVIn2());
+		//PROCESS SAMPLE
+	};
 
-		// PulseOut1(PulseIn1());
-		// PulseOut2(PulseIn2());
+	
 
-		// // Get switch position and set LEDs 0, 2, 4 accordingly
-		// int s = SwitchVal();
-		// LedOn(4, s == Switch::Down);
-		// LedOn(2, s == Switch::Middle);
-		// LedOn(0, s == Switch::Up);
+private:
 
-		// // Set LED 1, 3, 5 brightness to knob values
-		// LedBrightness(1, KnobVal(Knob::Main));
-		// LedBrightness(3, KnobVal(Knob::X));
-		// LedBrightness(5, KnobVal(Knob::Y));
+	int pulseTimer1 = 200;
+	int pulseTimer2 = 0;
+	int clockRate;
+	int clock = 0;
+
+
+	// random number generator
+	int32_t LFrnd()
+	{
+		static uint32_t lcg_seed = 1;
+		lcg_seed = 1664525 * lcg_seed + 1013904223;
+		return lcg_seed >> 21;
 	}
 };
 
 
 int main()
 {
-	Goldfish pt;
-	pt.Run();
+	Goldfish gf;
+	gf.EnableNormalisationProbe();
+	gf.Run();
 }
 
   
