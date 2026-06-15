2b71f0da4c6eff87d9d9cdb4aed59331ffe3ad54
soveda <161259864+soveda@users.noreply.github.com>
2026-06-15 11:42:18 +0100

    deleted build

--- releases/74_Wild_Pebble/MIDI_enabled/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/Users/adrianvos/Documents/GitHub/Workshop_Computer/releases/74_Wild_Pebble/MIDI_enabled/build/_deps/picotool/"
-};
