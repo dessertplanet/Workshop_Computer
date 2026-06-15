a7485e956168dfd3efa8958a183a03c0454e7b0c
soveda <161259864+soveda@users.noreply.github.com>
2026-06-15 11:37:33 +0100

    implementinf MIDI

---
+++ releases/74_Wild_Pebble/MIDI_enabled/build/CMakeFiles/WildPebbleMIDI.dir/Users/adrianvos/pico-sdk/lib/tinyusb/src/class/cdc/cdc_device.c.o.d
@@ -0,0 +1,6 @@
+CMakeFiles/WildPebbleMIDI.dir/Users/adrianvos/pico-sdk/lib/tinyusb/src/class/cdc/cdc_device.c.o: \
+ /Users/adrianvos/pico-sdk/lib/tinyusb/src/class/cdc/cdc_device.c \
+ /Users/adrianvos/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ /Users/adrianvos/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
+ /Users/adrianvos/Documents/GitHub/Workshop_Computer/releases/74_Wild_Pebble/MIDI_enabled/./tusb_config.h \
+ /Users/adrianvos/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
